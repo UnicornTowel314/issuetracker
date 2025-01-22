9c31e84d609193525698a88b0f9ad464607f8077
Sarah Mckee <sarahmckee714@gmail.com>
2025-01-22 12:02:28 -0600

    Added put, get, and delete requests

--- routes/api.js
+++ routes/api.js
@@ -7,17 +7,30 @@ module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
-      let project = req.params.project;
-      
+    .get(async function (req, res){
+      let projectName = req.params.project;
+      try {
+        const project = await ProjectModel.findOne({ name: projectName });
+        if (!project) {
+          return res.json({ error: "project not found" });
+        }else {
+          const issues = await IssueModel.find({ project: projectName, ...req.query });
+          if (!issues) {
+            return res.json([{ error: "no issues found" }]);
+          }
+          return res.json(issues);
+        }
+      }catch (err) {
+        console.log(err);
+        res.json({ error: "could not get", _id: _id });
+      }
     })
     
     .post(async function (req, res){
       let project = req.params.project;
       const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
       if (!issue_title || !issue_text || !created_by) {
-        res.json({ error: "required field(s) missing" });
-        return;
+        return res.json({ error: "required field(s) missing" });
       }
       
       if(!(await ProjectModel.exists({ name: project }))) {
@@ -50,14 +63,53 @@ module.exports = function (app) {
       }
     })
     
-    .put(function (req, res){
-      let project = req.params.project;
-      
+    .put(async function (req, res){
+      let projectName = req.params.project;
+      const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = req.body;
+      if (!_id) {
+        return res.json({ error: "missing _id" });
+      }
+
+      if (!issue_title && !issue_text && !created_by && !assigned_to && !status_text && !open) {
+        return res.json({ error: "no update field(s) sent", _id: _id });
+      }
+
+      try {
+        const projectModel = await ProjectModel.findOne({ name: projectName });
+        if (!projectModel) {
+          throw new Error("project not found");
+        }
+
+        let issue = await IssueModel.findByIdAndUpdate(_id, {
+          ...req.body,
+          updated_on: new Date()
+        });
+        await issue.save();
+        res.json({ result: "sucessfully updated", _id: _id });
+      }catch (err) {
+        console.log(err);
+        res.json({ error: "could not update", _id: _id });
+      }
     })
     
-    .delete(function (req, res){
-      let project = req.params.project;
-      
+    .delete(async function (req, res){
+      let projectName = req.params.project;
+      const { _id } = req.body;
+      if (!_id) {
+        return res.json({ error: "missing _id" });
+      }
+
+      try {
+        const deletedIssue = await IssueModel.findByIdAndDelete(_id);
+        if (!deletedIssue) {
+          return res.json({ error: "could not delete", _id: _id });
+        }
+
+        return res.json({ result: "successfully deleted", _id: _id });
+      } catch (err) {
+        console.log(err);
+        return res.json({ error: "internal server error", _id: _id });
+      }
     });
     
 };
